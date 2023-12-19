8eadadfa28121e59f4ae761170c7442c1b322ef1
StarlightSangdan <danju@starlight-sangdan.com>
2023-12-19 16:40:24 +0900

    fix: tsx module error

--- dist/MyComponent.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const MyComponent: () => import("react").JSX.Element;
-export default MyComponent;
---
+++ dist/components/MyComponent.d.ts
@@ -0,0 +1,3 @@
+import { FC } from "react";
+declare const MyComponent: FC;
+export default MyComponent;
